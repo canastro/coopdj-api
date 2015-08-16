0dee40e272b211c2f5ca8125f7a0a74d1fe54e28
Ricardo Canastro <ricardo.canastro@blip.pt>
2015-08-16 22:39:58 +0100

    Added access logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 # TODO: REFACTOR TO THIS: https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications
 import datetime
-from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request
 from flask.ext.mongoengine import MongoEngine
 from flask_restful import Api
 from app.resources.playlist import Playlist
 from app.resources.music import Music
 from app.models import db
 
+LOG_FILENAME = 'app_access_logs.log'
 # https://github.com/MongoEngine/flask-mongoengine/issues/77
 
 #Create our API
@@ -23,16 +26,49 @@ api = Api(app)
 api.add_resource(Playlist, '/playlist')
 api.add_resource(Music, '/musics/<int:music_id>')
 
-# Add CORS
+
+# After request, add CORS and log requests
+def getResponseLen(response):
+
+    try:
+        return len(response.data)
+    except:
+        return '-'
+
+def getRequestURL(request):
+    url = '\"' + request.method
+    url += ' /' + request.url.split(request.url_root)[1]
+    url += '\"'
+    return url
+
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+
+    app.logger.info('\t'.join([
+            request.remote_addr,                                            # client ip
+            '-',                                                            # RFC 1413 identity of the client.
+            '-',                                                            # user id
+            datetime.datetime.today().strftime('[%d/%b/%Y %H:%M:%S]'),      # date time
+            getRequestURL(request),
+            response.status.split(' ')[0],
+            getResponseLen(response.data)
+        ]))
+
     return response
 
 # Setup the database.
 def init(**config_overrides):
+
+    handler = RotatingFileHandler(
+        LOG_FILENAME,
+        maxBytes=1024 * 1024 * 100,
+        backupCount=20
+    )
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
 
     # apply overrides
     app.config.update(config_overrides)
